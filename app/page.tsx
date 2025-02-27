7d3b2fc6f0ebcc392a0b45920e7c4e40bd5cd981
manhdo-ikk <manhme>
2025-02-28 01:39:21 +0900

    Update: user.name

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        Hello hihi
+        Hello
       </div>
     </section>
   );
